6bc07587f7da87245c983a3f98acd71f3e5879ba
ajoy11s <ajoycse0811@gmail.com>
2024-10-26 09:37:12 +0600

    commit change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const router = express.Router();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
