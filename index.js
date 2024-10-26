24997d74b2fed582ad5df266a59dfa3feca32c29
ajoy11s <ajoycse0811@gmail.com>
2024-10-27 00:19:08 +0600

    commit change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const serverless = require('serverless-http');
+//const serverless = require('serverless-http');
 const router = express.Router();
 
 const port = process.env.PORT || 5000;
@@ -308,6 +308,6 @@ app.get('/', (req, res) => {
 // })
 
 
-app.use('/functions/api', router);
+//app.use('/functions/api', router);
 
-module.exports.handler = serverless(app);
+//module.exports.handler = serverless(app);
