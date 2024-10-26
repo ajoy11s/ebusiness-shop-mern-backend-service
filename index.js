c40356085fbc837990aaab7d5ee62b7078b03d83
ajoy11s <ajoycse0811@gmail.com>
2024-10-26 09:23:35 +0600

    commit change

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(express.json());
 //mongoDB Code added with function
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { error } = require('console');
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.q2fr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
+//const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.q2fr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const URI = process.env.MONGO_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -205,3 +205,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use('/functions/api', router);
+
+module.exports.handler = serverless(app);
